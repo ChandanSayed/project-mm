e1df6e2236daa9e1984d661a773a02991b041299
Mridul Islam <md.mridul.23@gmail.com>
2023-09-21 12:25:15 +0600

    type error updated

--- src/app/faqs/page.js
+++ src/app/faqs/page.js
@@ -20,7 +20,7 @@ const page = () => {
       <NavBar />
       <div className="bg-gradient-to-b from-blue-10 from-0% via-blue-0 via-60% to-white to-90% px-[15px] ">
         <div className="flex flex-col justify-center items-center gap-[30px] pt-[50px]">
-          <Image src={faqs} className="w-[128px] h-[128px] max-md:w-[80px] max-md:h-[80px]" />
+          <Image src={faqs} className="w-[128px] h-[128px] max-md:w-[80px] max-md:h-[80px]" alt="logo" />
           <h3 className="text-[40px] font-bold leading-normal max-md:text-[24px] px-6 text-center">How can we help you?</h3>
         </div>
 
@@ -31,7 +31,7 @@ const page = () => {
 
               <button className="absolute inset-y-0 right-0 flex items-center pr-3">
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
               </button>
             </label>
@@ -58,7 +58,7 @@ const page = () => {
           <div className="flex justify-center flex-col items-center h-[100vh]">
             <div className="max-w-[500px] h-[550px] max-lg:w-[100%] md:w-[100%] rounded-full opacity-[0.2]  bg-blue bg-gradient-to-b from-blue-10  blur-xl absolute z-10"></div>
             <div className=" bg-white border flex justify-center flex-col items-center max-w-[720px] max-lg:w-[100%] md:w-[100%] h-[400px] max-md:h-[370px] z-20">
-              <Image src={email2} className="w-[128px] h-[128px] relative bottom-[7.5rem] max-lg:bottom-28 max-md:bottom-[7rem] max-sm:bottom-24  max-md:w-[100px] max-md:h-[100px] " />
+              <Image src={email2} className="w-[128px] h-[128px] relative bottom-[7.5rem] max-lg:bottom-28 max-md:bottom-[7rem] max-sm:bottom-24  max-md:w-[100px] max-md:h-[100px] " alt="logo" />
               <div className="flex justify-center flex-col items-center relative bottom-10">
                 <h1 className="text-blue text-[26px] font-bold leading-normal text-center max-md:text-[17px] max-md:px-8 ">Still need help about a topic not listed?</h1>
                 <p className="text-base text-dark font-jetBrain max-md:text-[13px] max-md:text-center max-md:px-6 mb-[33px] mt-[21px]  ">A member of our team will respond to your enquiry shortly.</p>
