c37e85645b099d0555883deaef0656e6523d1c4f
Chandan Sayed <hangzhou635@gmail.com>
2023-10-09 03:26:37 +0600

    Homepage Link generate section added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,10 @@
+import LinkGenerate from './LinkGenerate';
 import hero from '/public/home/hero.jpeg';
 import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-b from-blue-10 from-0% via-blue-0 via-90% lg:to-white to-90% border-t border-black border-opacity-5 pt-[30px] px-[15px] lg:mb-[50px]">
+    <div className="bg-gradient-to-b from-blue-10 from-0% via-blue-0 via-90% lg:to-white to-90% border-t border-black border-opacity-5 pt-[30px] px-[15px] lg:mb-[50px] relative">
       <div style={{ backgroundImage: `url(${hero.src})` }} className={`hero-container relative max-w-[1320px] mx-auto overflow-hidden pt-[56px] lg:pt-[240px] bg-cover rounded-[20px_20px_0_0] lg:rounded-[20px] bg-center pb-[90px] sm:pb-[188px]`}>
         <div className="overlay bg-black bg-opacity-10 absolute inset-0"></div>
         <div className="hero-des text-center relative">
@@ -11,6 +12,7 @@ const Hero = () => {
           <p className="font-medium font-jetBrain max-w-[562px] mx-auto text-[15px] lg:text-xl mb-14 text-white">A reliable and trustworthy platform for discreet conversations between known parties.</p>
         </div>
       </div>
+      <LinkGenerate />
     </div>
   );
 };
--- src/components/LinkGenerate.js
+++ src/components/LinkGenerate.js
@@ -1,12 +1,63 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import lock from '/public/home/lock.png';
+import standard from '/public/home/standard.svg';
+import down from '/public/home/down.png';
+import reload from '/public/home/reload.png';
+import qr from '/public/home/qr.png';
+import copy from '/public/home/copy.png';
+import CloudflareTurnstile from './CloudflareTurnstile';
+
 const links = ['https://messagemoment.com/sqjgcf9', 'https://messagemoment.com/sqjgcf8', 'https://messagemoment.com/sqjgcf7', 'https://messagemoment.com/sqjgcf6'];
 
 const LinkGenerate = () => {
+  const [secure, setSecure] = useState(false);
   return (
     <>
-      <div className="top bg-blue">
-        <p className="pt-[19px] pb-[17px]">Click the button below to receive your random link that you can share with your friends any way you like!</p>
-        <div className="link border border-black border-opacity-10 mx-[15px] mt-[18px] mb-5 bg-white ">
-          <div className="flex "></div>
+      <div className="bg-blue max-w-[1096px] rounded-[20px] absolute -bottom-[112px] left-1/2 transform -translate-x-1/2 w-full mx-auto">
+        <p className="py-7 font-medium text-sm lg:text-[17px] max-w-[261px] lg:max-w-none text-white text-center">Click the button below to receive your random link that you can share with your friends any way you like!</p>
+        <div className="link bg-black bg-opacity-20 rounded-[0_0_20px_20px] pt-[18px] px-[15px]  lg:px-[30px] lg:pt-[35px] lg:pb-[25px] pb-5">
+          <div className="flex overflow-hidden">
+            <div className="flex items-center cursor-pointer rounded-[5px_0_0_5px] bg-white h-[50px]">
+              {secure ? (
+                <>
+                  <Image src={lock} alt="Lock" className="ml-3" /> <p className="mx-[10px] text-dark">Secure</p>
+                  <Image src={down} alt="down" className="mr-[22px]" />
+                </>
+              ) : (
+                <>
+                  <Image src={standard} alt="Lock" className="ml-3" /> <p className="mx-[10px] text-dark">Standard</p>
+                  <Image src={down} alt="down" className="mr-[22px]" />
+                </>
+              )}
+            </div>
+
+            <p className="flex-1 flex items-center font-jetBrain text-sm lg:text-[15px] rounded-[0_5px_5px_0] bg-white font-bold h-[50px]">https://messagemoment.com/sqjgcf9</p>
+            <div className="buttons pl-[6px] flex gap-[5px]">
+              <button disabled className="cursor-pointer h-[50px] flex items-center justify-center border-[1.5px] border-white border-opacity-20 rounded-[5px] px-[15px] " title="reload">
+                <Image src={reload} alt="reload" />
+              </button>
+              <button className="cursor-pointer h-[50px] flex items-center justify-center border-[1.5px] border-white border-opacity-20 rounded-[5px] px-[15px] bg-white">
+                <Image src={qr} alt="qr" />
+              </button>
+              <button className="cursor-pointer h-[50px] flex items-center justify-center border-[1.5px] border-white border-opacity-20 rounded-[5px] px-[15px] bg-white">
+                <Image src={copy} alt="copy" />
+              </button>
+            </div>
+          </div>
+          <div className="flex gap-5 mt-5">
+            <div className="w-[300px]">
+              <CloudflareTurnstile />
+            </div>{' '}
+            <button className="bg-white h-[65px] text-blue hover:text-opacity-90 grow font-jetBrain font-bold text-[14px] rounded-md lg:text-[15px] flex items-center justify-center">Open Chat</button>
+          </div>
+          <p className="text-[11px] lg:text-[13px] font-medium text-white text-center mt-[15px]">
+            By starting this chat session, you agree to our <Link href={`/terms`}>Terms of Use</Link> and <Link href={`/privacy`}>Privacy Policy</Link>, and that you and everybody you share the chat link with is above 16 years of age.
+          </p>
         </div>
       </div>
     </>
