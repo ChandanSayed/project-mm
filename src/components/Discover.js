26f88b36eef6a7cf6054c2c3eb3461adc475232d
Chandan Sayed <hangzhou635@gmail.com>
2023-10-08 20:52:10 +0600

    Home Page added

---
+++ src/components/Discover.js
@@ -0,0 +1,50 @@
+import phone from '/public/home/cellphone.png';
+import laptop from '/public/home/laptop.png';
+import tablet from '/public/home/tablet.png';
+import television from '/public/home/television.png';
+import controller from '/public/home/controller.png';
+import Image from 'next/image';
+import DiscoverSlider from './DiscoverSlider';
+
+const Discover = () => {
+  return (
+    <div className="bg-dark px-[15px]">
+      <div className="max-w-[1320px] mx-auto">
+        <h2 className="font-bold text-[34px] text-center lg:text-[60px] tracking-[-.5px] lg:tracking-normal sm:max-w-[450px] lg:max-w-[934px] mx-auto text-white lg:pt-[70px] lg:pb-10">Discover more from MessageMoment</h2>
+        <DiscoverSlider />
+        <p className="text-white text-[26px] text-center mt-10 lg:mt-[60px] mb-10">
+          Use MessageMoment on any device via Browser<sup className="text-[60%]">1</sup>
+        </p>
+        <div className="flex justify-center gap-6 lg:gap-12 pb-10 lg:pb-[58px]">
+          <div className="device text-center">
+            <Image src={phone} className="w-[36px] h-[36px] mb-5 lg:h-[48px] lg:w-[48px]" alt="Device" />
+            <p className="text-white">Phone</p>
+          </div>
+          <div className="device text-center">
+            <Image src={laptop} className="w-[36px] h-[36px] mb-5 lg:h-[48px] lg:w-[48px]" alt="Device" />
+            <p className="text-white">Desktop</p>
+          </div>
+          <div className="device text-center">
+            <Image src={tablet} className="w-[36px] h-[36px] mb-5 lg:h-[48px] lg:w-[48px]" alt="Device" />
+            <p className="text-white">Tablet</p>
+          </div>
+          <div className="device text-center">
+            <Image src={television} className="w-[36px] h-[36px] mb-5 lg:h-[48px] lg:w-[48px]" alt="Device" />
+            <p className="text-white">Smart Tv</p>
+          </div>
+          <div className="device text-center">
+            <Image src={controller} className="w-[36px] h-[36px] mb-5 lg:h-[48px] lg:w-[48px]" alt="Device" />
+            <p className="text-white">Consoles</p>
+          </div>
+        </div>
+        <div className="disclaimer max-w-[1096px] mx-auto pt-5 border-t border-t-white border-opacity-20 pb-9 text-center">
+          <p className="text-[11px] text-white lg:text-[13px] font-medium">
+            <sup className="text-[60%]">1</sup>Disclaimer: Compatibility may vary across different devices and operating systems.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -8,8 +8,8 @@ import { useState } from 'react';
 
 const Steps = () => {
   const steps = [
-    { id: 1, img: one, step: 'Step 1', stepName: 'Get Started' },
-    { id: 2, img: two, step: 'Step 2', stepName: 'Share Link' },
+    { id: 1, img: one, step: 'Step 1', stepName: 'Generate Link' },
+    { id: 2, img: two, step: 'Step 2', stepName: 'Share It' },
     { id: 3, img: three, step: 'Step 3', stepName: 'Set Expiry' },
     { id: 4, img: four, step: 'Step 4', stepName: 'End Session' }
   ];
@@ -20,7 +20,7 @@ const Steps = () => {
     <div className="steps-desktop flex gap-5">
       {steps.map((step, i) => {
         return (
-          <div className={`step bg-white rounded-[20px] relative p-[30px] cursor-pointer ${index === i ? 'sm:w-[220px] lg:flex-1' : 'sm:w-[220px] lg:w-[260px] bg-opacity-20 text-center'}`} kay={step.id} onMouseEnter={() => setIndex(i)}>
+          <div className={`step bg-white rounded-[20px] relative p-[30px] cursor-pointer ${index === i ? 'sm:w-[220px] lg:flex-1' : 'sm:w-[220px] lg:w-[260px] bg-opacity-20 text-center'}`} key={step.id} onMouseEnter={() => setIndex(i)}>
             <h2 className={`font-bold text-[34px] lg:text-[60px] absolute left-[30px] top-4 ${index === i ? 'text-blue' : 'text-white opacity-20'}`}>{step.id}</h2>
             <Image src={step.img} alt="step" className="w-[60px] h-[60px] lg:w-[80px] lg:h-[80px] mt-[30px] sm:ml-[45px] lg:ml-[90px] mb-5" />
             <span className={`font-jetBrain text-[13px]  ${index === i ? 'text-darkGrey' : 'text-white'}`}>{step.step}</span>
