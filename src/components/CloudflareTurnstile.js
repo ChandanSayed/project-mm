9bdc1144346426bdc85b730503ba5bee8f77249f
Chandan Sayed <hangzhou635@gmail.com>
2023-10-18 19:10:47 +0600

    Contact page popup added2

--- src/components/CloudflareTurnstile.js
+++ src/components/CloudflareTurnstile.js
@@ -1,23 +1,23 @@
 import Script from 'next/script';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 const CloudflareTurnstile = () => {
-  useEffect(() => {
-    // const turnstileCallbackFunction = function () {
-    //   const turnstileOptions = {
-    //     sitekey: '0x4AAAAAAAKTDe6f-KqwG_iW',
-    //     callback: function (token) {
-    //       console.log(`Challenge Success: ${token}`);
-    //     }
-    //   };
-    //   turnstile.render('#container', turnstileOptions);
-    // };
-    // if (typeof window !== 'undefined') {
-    //   // Ensure that the script is executed only on the client-side
-    //   turnstileCallbackFunction();
-    // }
-    // handleRequest();
-  }, []);
+  // useEffect(() => {
+  // const turnstileCallbackFunction = function () {
+  //   const turnstileOptions = {
+  //     sitekey: '0x4AAAAAAAKTDe6f-KqwG_iW',
+  //     callback: function (token) {
+  //       console.log(`Challenge Success: ${token}`);
+  //     }
+  //   };
+  //   turnstile.render('#container', turnstileOptions);
+  // };
+  // if (typeof window !== 'undefined') {
+  //   // Ensure that the script is executed only on the client-side
+  //   turnstileCallbackFunction();
+  // }
+  // handleRequest();
+  // }, []);
 
   // async function handleRequest() {
   //   // ... Receive token
