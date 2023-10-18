cdcf24a300be27659a16b0f906d0e8b4a4de5c54
Chandan Sayed <hangzhou635@gmail.com>
2023-10-18 18:48:20 +0600

    Contact page popup added

--- src/components/ReturnToHomepage.js
+++ src/components/ReturnToHomepage.js
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 
-const ReturnToHomepage = () => {
+const ReturnToHomepage = ({ text = 'Return to Homepage' }) => {
   return (
     <div>
       <Link href="/">
         <button className={`bg-blue shadow w-[236px] h-[46px] hover:bg-opacity-80 focus:shadow-outline focus:outline-none text-white py-2 px-4 rounded-md font-jetBrain text-[14px] lg:text-[15px] font-bold`} type="button">
-          Return to Homepage
+          {text}
         </button>
       </Link>
     </div>
