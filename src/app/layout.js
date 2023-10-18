5f7e6dcd0be7411622e26178f97e59e955891569
Chandan Sayed <hangzhou635@gmail.com>
2023-10-18 15:22:00 +0600

    Scroll functionality added to homepage start buttons

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="text-dark">
+      <body className="text-dark scroll-smooth">
         <AppContextProvider>{children}</AppContextProvider>
       </body>
     </html>
--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -1,5 +1,9 @@
-const HeaderButton = ({ classes, name }) => {
-  return <button className={`rounded-md h-[46px] w-[150px] ${classes} px-5 hover:bg-opacity-80 text-base font-jetBrain font-extrabold`}>{name}</button>;
+const HeaderButton = ({ classes, name, event = () => console.log('Clicked!') }) => {
+  return (
+    <button onClick={event} className={`rounded-md h-[46px] w-[150px] ${classes} px-5 hover:bg-opacity-80 text-base font-jetBrain font-extrabold`}>
+      {name}
+    </button>
+  );
 };
 
 export default HeaderButton;
