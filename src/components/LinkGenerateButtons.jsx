9f67de291a2ff26b25992345f580e6bbee3efcea
Chandan Sayed <hangzhou635@gmail.com>
2023-10-20 01:37:28 +0600

    mobile hover state off

--- src/components/LinkGenerateButtons.jsx
+++ src/components/LinkGenerateButtons.jsx
@@ -7,7 +7,7 @@ const LinkGenerateButtons = ({ showGenerate, openChat, showQr, generatedLink, sh
       <div className="relative">
         <Tooltip showCopy={showGenerate} message={`Regenerate`} />
         {openChat ? (
-          <button onMouseEnter={handleGenerateTip} onMouseLeave={handleGenerateTipLeave} onClick={secure ? generateLinkSecure : generateLink} className="cursor-pointer h-[50px] w-[50px] flex items-center justify-center border-[1.5px] border-white rounded-[5px] hover:bg-dark md:hover:bg-opacity-50">
+          <button onMouseEnter={handleGenerateTip} onMouseLeave={handleGenerateTipLeave} onClick={secure ? generateLinkSecure : generateLink} className="cursor-pointer h-[50px] w-[50px] flex items-center justify-center border-[1.5px] border-white rounded-[5px] md:hover:bg-dark md:hover:bg-opacity-50">
             <svg xmlns="http://www.w3.org/2000/svg" width="23" height="22" viewBox="0 0 23 22" fill="none">
               <path d="M21.0374 9.74766C20.7312 7.54382 19.7088 5.50181 18.1278 3.93618C16.5468 2.37055 14.4949 1.36816 12.2882 1.08342C10.0814 0.79868 7.84231 1.24739 5.91568 2.36042C3.98906 3.47345 2.48183 5.18907 1.62617 7.24298M1 2.23363V7.24298H6.00936M1 12.2523C1.30627 14.4562 2.32865 16.4982 3.90964 18.0638C5.49063 19.6295 7.54252 20.6318 9.74925 20.9166C11.956 21.2013 14.1951 20.7526 16.1217 19.6396C18.0484 18.5265 19.5556 16.8109 20.4113 14.757M21.0374 19.7664V14.757H16.0281" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
