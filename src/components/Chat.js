a35e39314e69ba1057e510067e6875d9e3221d6d
Chandan Sayed <hangzhou635@gmail.com>
2023-11-05 13:47:47 +0600

    Chat input position changed 2

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -148,7 +148,7 @@ const Chat = ({ children, showMenu }) => {
   };
   return (
     <>
-      <div className="grid grid-cols-1 xl:grid-cols-[1fr_220px] gap-[30px] lg:mt-6 max-w-[1440px] mx-auto bg-white pb-5">
+      <div className="grid grid-cols-1 xl:grid-cols-[1fr_220px] gap-3 lg:mt-6 max-w-[1440px] mx-auto bg-white pb-5">
         <div className="bg-white max-lg:h-[calc(100vh-200px)] max-lg:pb-10 h-[calc(100vh-136px)]">
           <div className="h-full overflow-y-auto custom-scrollbar flex flex-col justify-between">
             {/* {chats[1].joinedMessage.map(chat => (
@@ -156,7 +156,7 @@ const Chat = ({ children, showMenu }) => {
                   <SidebarComponents color={chat.color} contentLeft={chat.name} text />
                 </div>
               ))} */}
-            <div>
+            <div className="h-[calc(100%-144px)] overflow-y-auto custom-scrollbar">
               <ChatSticky />
               <div className="pb-8 px-4">
                 <div className=" bg-midGray px-4 py-5 rounded-lg mt-[15px] grid grid-cols-1 lg:grid-cols-5">
