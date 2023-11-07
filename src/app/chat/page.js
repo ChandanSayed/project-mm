e2111c326c239aac8b8086c20346fefcbf72d20a
Chandan Sayed <hangzhou635@gmail.com>
2023-11-07 12:43:52 +0600

    Overflow hidden added to the chat

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -7,10 +7,10 @@ import ChatHeader from '@/components/ChatHeader';
 const page = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <>
+    <div className="overflow-hidden">
       <ChatHeader setShowMenu={setShowMenu} showMenu={showMenu} />
       <Chat showMenu={showMenu} />
-    </>
+    </div>
   );
 };
 
