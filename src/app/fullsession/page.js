7ee952910f74192d75e45b185f0689dcf161a36f
Chandan Sayed <hangzhou635@gmail.com>
2023-11-28 03:12:35 +0600

    Full session full screen

--- src/app/fullsession/page.js
+++ src/app/fullsession/page.js
@@ -9,8 +9,8 @@ const page = () => {
     <>
       <LogoHeader />
       <div className="lg:bg-gradient-to-b from-blue-10 from-0% via-blue-0 via-60% to-white to-90% border-t border-black border-opacity-10">
-        <div className="max-w-[720px] mx-auto">
-          <BlurSection img={session} pt="pt-[105px] lg:pt-[134px]" heading={`The chat session is full! There are currently 10/10 users joined`} description={`Unfortunately you are unable to enter the chat at this time.`} padding={'pt-[239px] pb-[218px] lg:pt-[258px] lg:pb-[224px]'} />
+        <div className="max-w-[720px] mx-auto flex items-center justify-center h-[calc(100vh-175px)]">
+          <BlurSection img={session} pt="pt-[105px] lg:pt-[134px]" heading={`The chat session is full! There are currently 10/10 users joined`} description={`Unfortunately you are unable to enter the chat at this time.`} padding={'w-full'} />
         </div>
       </div>
       <NavLessPageFooter />
