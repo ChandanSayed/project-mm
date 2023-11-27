865961b7e138e5e6e219665c9ade3536f0d7214e
Chandan Sayed <hangzhou635@gmail.com>
2023-11-28 03:09:24 +0600

    Expired session full screen 2

--- src/app/expired/page.js
+++ src/app/expired/page.js
@@ -11,7 +11,7 @@ const page = () => {
       <LogoHeader />
       <div className="lg:bg-gradient-to-b from-blue-10 from-0% via-blue-0 via-60% to-white to-90% border-t border-black border-opacity-10">
         <div className="max-w-[720px] mx-auto flex items-center justify-center h-[calc(100vh-175px)]">
-          <BlurSection img={error} pt="pt-[85px] lg:pt-[108px]" heading={`The chat session you were invited to with this link is no longer available`} description={`Return to the homepage to generate a new chat session.`} padding={''} sessionLink={true} />
+          <BlurSection img={error} pt="pt-[85px] lg:pt-[108px]" heading={`The chat session you were invited to with this link is no longer available`} description={`Return to the homepage to generate a new chat session.`} padding={'w-full'} sessionLink={true} />
         </div>
       </div>
       <NavLessPageFooter />
